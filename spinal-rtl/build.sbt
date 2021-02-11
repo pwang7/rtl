37915d0dae28cbef8f04cc77a3a7f2735ad49dbd
Pu Wang <nicolas.weeks@gmail.com>
2021-02-11 15:14:29 +0800

    add scala test

--- spinal-rtl/build.sbt
+++ spinal-rtl/build.sbt
@@ -6,10 +6,10 @@ scalaVersion := "2.11.12"
 val spinalVersion = "1.4.3"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" % "scalatest_2.11" % "2.2.1",
+  "org.scalatest" %% "scalatest" % "3.0.8" % "test",
   "com.github.spinalhdl" % "spinalhdl-core_2.11" % spinalVersion,
   "com.github.spinalhdl" % "spinalhdl-lib_2.11"  % spinalVersion,
-   compilerPlugin("com.github.spinalhdl" % "spinalhdl-idsl-plugin_2.11" % spinalVersion)
+  compilerPlugin("com.github.spinalhdl" % "spinalhdl-idsl-plugin_2.11" % spinalVersion),
 )
 
 fork := true
--- spinal-rtl/src/test/scala/CounterTest.scala
+++ spinal-rtl/src/test/scala/CounterTest.scala
@@ -1,23 +1,18 @@
+import org.scalatest.FunSuite
 import spinal.sim._
 import spinal.core._
 import spinal.core.sim._
-import java.security.KeyStore.TrustedCertificateEntry
 
-object TbCounter extends App{
+class CounterTest extends FunSuite {
   val CNT_WIDTH = 4
   val CNT_MAX = scala.math.pow(2, CNT_WIDTH).toInt - 1
 
-  SimConfig
-    .withWave
-    //   .withConfig(SpinalConfig(
-    //     defaultClockDomainFrequency = FixedFrequency(100 MHz),
-    //     defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC)))
-    .compile(new Counter(CNT_WIDTH))
-    .doSim{dut=>
+  val dut=SimConfig.withWave.withCoverage.compile(new Counter(CNT_WIDTH))
+
+  test("Free running counter") {
+    dut.doSim { dut=>
       dut.clockDomain.forkStimulus(10)
-    //   dut.clockDomain.assertReset()
-    //   dut.clockDomain.waitSampling()
-    //   dut.clockDomain.deassertReset()
+
       dut.io.clear #= true
       dut.clockDomain.waitSampling()
       dut.io.clear #= false
@@ -36,6 +31,23 @@ object TbCounter extends App{
         assert(curCnt == i, s"Counter value wrong after overflow, expect cnt=$i, actual cnt=$curCnt")
         dut.clockDomain.waitSampling()
       }
+    }
+  }
+
+  test("Clear counter") {
+    dut.doSim { dut=>
+      dut.clockDomain.forkStimulus(10)
+
+      dut.clockDomain.assertReset()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.deassertReset()
+
+      for(i <- 0 to CNT_MAX / 2) {
+        val curCnt = dut.io.value.toInt
+        println(s"curCnt=$curCnt, i=$i")
+        assert(curCnt == i, s"Counter value wrong after reset, expect cnt=$i, actual cnt=$curCnt")
+        dut.clockDomain.waitSampling()
+      }
 
       dut.io.clear #= true
       dut.clockDomain.waitSampling()
@@ -49,4 +61,5 @@ object TbCounter extends App{
         dut.clockDomain.waitSampling()
       }
     }
+  }
 }
