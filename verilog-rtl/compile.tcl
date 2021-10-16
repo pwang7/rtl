9bcd022ce087e0aa6850b8518d779ab52efefa41
Pu Wang <nicolas.weeks@gmail.com>
2021-10-16 16:32:45 +0800

    enable verilog lint

--- verilog-rtl/compile.tcl
+++ verilog-rtl/compile.tcl
@@ -1,26 +1,30 @@
 #=====================================================
 # Step 0: Clean up
 #=====================================================
-remove_design -hierarchy
+remove_design -all
 
 #=====================================================
 # Step 1: Read & eleaborate the RTL file list & check
 #=====================================================
 set TOP_MODULE $env(TOP_MODULE)
-#analyze -format verilog [list axi_dma.v DmaMem.v define.v]
-#elaborate      $TOP_MODULE -architecture verilog
-read_file $RTL_PATH -autoread -recursive -format verilog -top $TOP_MODULE
+analyze -recursive -autoread $RTL_PATH
+#analyze -format sverilog -recursive -autoread $RTL_PATH
+elaborate      $TOP_MODULE
+#set_app_var verilogout_no_tri true
+#set_fix_multiple_port_nets -all -buffer_constants [get_designs * -hierarchy]
+write_file -format verilog -no_implicit -hierarchy -output $MAPPED_PATH/${TOP_MODULE}.v;
+#read_file $RTL_PATH -autoread -recursive -format verilog -top $TOP_MODULE
 current_design $TOP_MODULE
 
 # 防止忘记设置link library,或者set错。其实不需要，但是为了强化
 if {[link] == 0} {
-	echo "Link with error!";
-	exit;
+	echo "Link with error!"
+	exit
 }
 
 if {[check_design] == 0} {
-	echo "Check design with error!";
-	exit;
+	echo "Check design with error!"
+	exit
 }
 
 #=====================================================
@@ -40,31 +44,66 @@ write -f ddc -hierarchy -output ${UNMAPPED_PATH}/{TOP_MODULE}.ddc
 #=====================================================
 # Step 4: Define clock
 #=====================================================
-set  CLK_NAME          $env(CLK_NAME)
-set  CLK_PERIOD        $env(CLK_PERIOD)
-set  CLK_SKEW          [expr  $CLK_PERIOD * 0.05]
-set  CLK_TRAN          [expr  $CLK_PERIOD * 0.01]
-set  CLK_SRC_LATENCY   [expr  $CLK_PERIOD * 0.1]
-set  CLK_LATENCY       [expr  $CLK_PERIOD * 0.1]
+set  CLK_NAME          [split $env(CLK_NAME) ,]
+set  CLK_PERIOD        [split $env(CLK_PERIOD) ,]
+set  CLK_NUM           [llength $CLK_NAME]
+set  PRD_NUM           [llength $CLK_PERIOD]
+puts "clocks: $CLK_NAME, clock #=$CLK_NUM, periods: $CLK_PERIOD, period #=$PRD_NUM"
+#set  CLK_SKEW          [expr  $CLK_PERIOD * 0.05]
+#set  CLK_TRAN          [expr  $CLK_PERIOD * 0.01]
+#set  CLK_SRC_LATENCY   [expr  $CLK_PERIOD * 0.1]
+#set  CLK_LATENCY       [expr  $CLK_PERIOD * 0.1]
 
-create_clock   -period   $CLK_PERIOD      [get_ports  $CLK_NAME]
-set_ideal_network      [get_ports  $CLK_NAME];  #这两句是废话，不过强化clk是ideal_network
-set_dont_touch_network [get_ports  $CLK_NAME];  #告诉DC不对clk做优化
-set_drive    0         [get_ports  $CLK_NAME];  #设置驱动，0代表无穷大
-set_clock_uncertainty  -setup   $CLK_SKEW        [get_ports  $CLK_NAME];  #偏移
-set_clock_transition   -max     $CLK_TRAN        [get_ports  $CLK_NAME];  #斜率反转，非直角翻转
-set_clock_latency -source -max  $CLK_SRC_LATENCY [get_ports  $CLK_NAME];  #PCB版上晶振到芯片引脚的延迟
-set_clock_latency -max          $CLK_LATENCY     [get_ports  $CLK_NAME];  #芯片引脚到内部触发器的延迟
+if {$CLK_NUM != $PRD_NUM} {
+    echo "the length of CLK_NAME and CLK_PERIOD not equal"
+    exit
+}
+
+lmap CLK_N $CLK_NAME CLK_P $CLK_PERIOD {
+    set  CLK_SKEW          [expr  $CLK_P* 0.05]
+    set  CLK_TRAN          [expr  $CLK_P * 0.01]
+    set  CLK_SRC_LATENCY   [expr  $CLK_P * 0.1]
+    set  CLK_LATENCY       [expr  $CLK_P * 0.1]
+
+    create_clock -period $CLK_P [get_ports  $CLK_N]
+
+    set_ideal_network      [get_ports  $CLK_N];  #这两句是废话，不过强化clk是ideal_network
+    set_dont_touch_network [get_ports  $CLK_N];  #告诉DC不对clk做优化
+    set_drive    0         [get_ports  $CLK_N];  #设置驱动，0代表无穷大
+    set_clock_uncertainty  -setup   $CLK_SKEW        [get_ports  $CLK_N];  #偏移
+    set_clock_transition   -max     $CLK_TRAN        [get_ports  $CLK_N];  #斜率反转，非直角翻转
+    set_clock_latency -source -max  $CLK_SRC_LATENCY [get_ports  $CLK_N];  #PCB版上晶振到芯片引脚的延迟
+    set_clock_latency -max          $CLK_LATENCY     [get_ports  $CLK_N];  #芯片引脚到内部触发器的延迟
+}
+error_info
+
+if {[llength $CLK_NAME] > 1} {
+    set DESIGN_CLOCKS [all_clocks]
+    foreach_in_collection CLK_N $DESIGN_CLOCKS {
+        set_false_path -from $CLK_N -to [remove_from_collection [all_clocks] $CLK_N]
+    }
+}
+
+#create_clock   -period   $CLK_PERIOD      [get_ports  $CLK_NAME]
+#set_ideal_network      [get_ports  $CLK_NAME];  #这两句是废话，不过强化clk是ideal_network
+#set_dont_touch_network [get_ports  $CLK_NAME];  #告诉DC不对clk做优化
+#set_drive    0         [get_ports  $CLK_NAME];  #设置驱动，0代表无穷大
+#set_clock_uncertainty  -setup   $CLK_SKEW        [get_ports  $CLK_NAME];  #偏移
+#set_clock_transition   -max     $CLK_TRAN        [get_ports  $CLK_NAME];  #斜率反转，非直角翻转
+#set_clock_latency -source -max  $CLK_SRC_LATENCY [get_ports  $CLK_NAME];  #PCB版上晶振到芯片引脚的延迟
+#set_clock_latency -max          $CLK_LATENCY     [get_ports  $CLK_NAME];  #芯片引脚到内部触发器的延迟
 
 
 #=====================================================
 # Step 4: Define reset
 #=====================================================
-set  RST_NAME          $env(RST_NAME)
-set_ideal_network      [get_ports $RST_NAME]
-set_dont_touch_network [get_ports $RST_NAME]
-set_drive    0         [get_ports $RST_NAME]
-
+set  RST_NAME          [split $env(RST_NAME) ,]
+#set  RST_NAME          $env(RST_NAME)
+foreach RST_N $RST_NAME {
+    set_ideal_network      [get_ports $RST_N]
+    set_dont_touch_network [get_ports $RST_N]
+    set_drive    0         [get_ports $RST_N]
+}
 #=====================================================
 # Step 5: Set input delay (Using timing budget)
 # Assume a weak cell to drive the inputs pins
@@ -73,11 +112,14 @@ set_drive    0         [get_ports $RST_NAME]
 #set   DRIVE_CELL        INVX1
 #set   DRIVE_PIN         Y
 set   TIME_BUDGET        0.4
-set   ALL_IN_EXCEPT_CLK [remove_from_collection [all_inputs] [get_ports "$CLK_NAME"]]
-set   INPUT_DELAY       [expr  $CLK_PERIOD * (1 - $TIME_BUDGET)]
-
-set_input_delay     $INPUT_DELAY     -clock  $CLK_NAME   $ALL_IN_EXCEPT_CLK; #相对CLK_NAME对哪些端口设置延迟
-#set_input_delay    -min   0    -clock $CLK_NAME   $ALL_IN_EXCEPT_CLK
+set   ALL_IN_EXCEPT_CLK [remove_from_collection [all_inputs] [get_ports $CLK_NAME]]
+#set   INPUT_DELAY       [expr  $CLK_PERIOD * (1 - $TIME_BUDGET)]
+#set_input_delay     $INPUT_DELAY     -clock  $CLK_NAME   $ALL_IN_EXCEPT_CLK; #相对CLK_NAME对哪些端口设置延迟
+lmap CLK_N $CLK_NAME CLK_P $CLK_PERIOD {
+    set INPUT_DELAY [expr  $CLK_P * (1 - $TIME_BUDGET)]
+    set_input_delay $INPUT_DELAY -clock $CLK_N $ALL_IN_EXCEPT_CLK; #相对CLK_NAME对哪些端口设置延迟
+    #set_input_delay    -min   0    -clock $CLK_N   $ALL_IN_EXCEPT_CLK
+}
 set_driving_cell    -lib_cell ${DRIVE_CELL}  -pin ${DRIVE_PIN} -library $LIB_NAME $ALL_IN_EXCEPT_CLK 
 # 给某些端口加库里某个cell的某个pin作为负载
 
@@ -85,11 +127,14 @@ set_driving_cell    -lib_cell ${DRIVE_CELL}  -pin ${DRIVE_PIN} -library $LIB_NAM
 #=====================================================
 # Step 6: Set output delay
 #=====================================================
-set   OUTPUT_DELAY     [expr  $CLK_PERIOD * (1 - $TIME_BUDGET)]
+#set   OUTPUT_DELAY     [expr  $CLK_PERIOD * (1 - $TIME_BUDGET)]
 #set   MAX_LOAD         [expr  [load_of $LIB_NAME/INVX8/A] * 10]
 set   MAX_LOAD         [expr  [load_of $LOAD_CELL] * 10]
-
-set_output_delay        $OUTPUT_DELAY    -clock   $CLK_NAME [all_outputs]
+#set_output_delay        $OUTPUT_DELAY    -clock   $CLK_NAME [all_outputs]
+lmap CLK_N $CLK_NAME CLK_P $CLK_PERIOD {
+    set OUTPUT_DELAY [expr $CLK_P * (1 - $TIME_BUDGET)]
+    set_output_delay $OUTPUT_DELAY -clock $CLK_N [all_outputs]
+}
 set_load                [expr  $MAX_LOAD * 3]  [all_outputs]
 # 针对输出单元，插入隔离单元，把外部端口和内部电路隔离开
 set_isolate_ports       -type buffer           [all_outputs]
@@ -104,8 +149,8 @@ set_isolate_ports       -type buffer           [all_outputs]
 #set_output_delay       [expr $CLK_PERIOD * 0.1] -clock $CLK_NAME -add_delay [get_ports y_o]
 
 if {[check_timing] == 0} {
-	echo "check timing with error!";
-	exit;
+	echo "check timing with error!"
+	exit
 }
 
 #=====================================================
@@ -113,13 +158,14 @@ if {[check_timing] == 0} {
 #=====================================================
 #set   WIRE_LOAD_MODEL         tsmc13_wl50
 #set   OPERA_CONDITION         slow
-set_operating_conditions      -max   $OPERA_CONDITION \
-							  -max_library  $LIB_NAME;  #设置工作条件
-set auto_wire_load_selection  false;           #DC不采用自动线负载模型
+set_operating_conditions      -max   $OPERA_CONDITION -max_library  $LIB_NAME;  #设置工作条件
 set_wire_load_mode            top
-set_wire_load_model           -name  $WIRE_LOAD_MODEL \
-							  -library      $LIB_NAME;  #设置线负载模型
-							
+if {[string is false $WIRE_LOAD_MODEL]} {
+    set auto_wire_load_selection  true;           #DC采用自动线负载模型
+} else {
+    set auto_wire_load_selection  false;          #显式设置wire_load_model，DC不采用自动线负载模型
+    set_wire_load_model           -name  $WIRE_LOAD_MODEL -library      $LIB_NAME;  #设置线负载模型
+}
 #=====================================================
 # Step 9: Set area constraint (Let's DC try its best)
 #=====================================================
@@ -137,16 +183,23 @@ set_max_area   0;  #期望面积最小为0
 # Step 11: Set DRC constraint
 # Avoid getting stack on one path （遇到violation才使用这个部分，如果加了还是违例，就得修改代码了）
 #=====================================================
-group_path   -name   $CLK_NAME  -weight 5 \
-								-critical_range   [expr $CLK_PERIOD * 0.1]
-#指定一个group名字，指定一个权重（路径差，需要尽最大优化）\指定一个范围，对这个范围内的路径进行优化，一般不超过周期%10。通常分为下面几个组：输入，输出，之间的路径							
-group_path   -name   INPUTS     -from [all_inputs] \
-								-critical_range   [expr $CLK_PERIOD * 0.1]
-group_path   -name   OUTPUTS    -to [all_outputs] \
-								-critical_range   [expr $CLK_PERIOD * 0.1]
-group_path   -name   COMB       -from [all_inputs] \
-								-to [all_outputs] \
-								-critical_range   [expr $CLK_PERIOD * 0.1]	
+lmap CLK_N $CLK_NAME CLK_P $CLK_PERIOD {
+    group_path -name $CLK_N -weight 5 -critical_range [expr $CLK_P * 0.1]
+    #指定一个group名字，指定一个权重（路径差，需要尽最大优化）指定一个范围，对这个范围内的路径进行优化，一般不超过周期%10。通常分为下面几个组：输入，输出，之间的路径
+    group_path -name "INPUTS_$CLK_N"  -from [all_inputs]  -critical_range [expr $CLK_P * 0.1]
+    group_path -name "OUTPUTS_$CLK_N" -to   [all_outputs] -critical_range [expr $CLK_P * 0.1]
+    group_path -name "COMB_$CLK_N"    -from [all_inputs]  -to [all_outputs] -critical_range [expr $CLK_P * 0.1]	
+}
+#group_path   -name   $CLK_NAME  -weight 5 \
+#    -critical_range   [expr $CLK_PERIOD * 0.1]
+##指定一个group名字，指定一个权重（路径差，需要尽最大优化）指定一个范围，对这个范围内的路径进行优化，一般不超过周期%10。通常分为下面几个组：输入，输出，之间的路径
+#group_path   -name   INPUTS     -from [all_inputs] \
+#    -critical_range   [expr $CLK_PERIOD * 0.1]
+#group_path   -name   OUTPUTS    -to [all_outputs] \
+#    -critical_range   [expr $CLK_PERIOD * 0.1]
+#group_path   -name   COMB       -from [all_inputs] \
+#    -to [all_outputs] \
+#    -critical_range   [expr $CLK_PERIOD * 0.1]	
 report_path_group								
 
 
@@ -185,10 +238,11 @@ set_fix_multiple_port_nets  -all  -buffer_constants;     #端口连端口加buff
 #=====================================================
 # Step 14: compile flow
 #=====================================================
-set_host_options -max_cores 8
+set_host_options -max_cores 16; #8 might be max
 set_optimize_registers true; #流水线寄存器优化
 #ungrouop -flatten -all;  #不以module形式显示
-compile_ultra -timing -retime -scan
+#compile_ultra -timing -retime -scan
+compile -map_effort high -area_effort medium
 # lst-pass compile
 #compile -map_effort high -area_effort medium
 #compile -map_effort medium -area_effort high -boundary_optimization
@@ -240,10 +294,17 @@ redirect  -tee -file ${REPORT_PATH}/report_constraint.txt   {report_constraint -
 redirect  -tee -file ${REPORT_PATH}/check_setup.txt    {report_timing -delay_type max}
 redirect  -tee -file ${REPORT_PATH}/check_hold.txt     {report_timing -delay_type min}
 redirect  -tee -file ${REPORT_PATH}/check_fanout.txt   {report_timing -net}
+redirect  -tee -file ${REPORT_PATH}/report_timing.txt  {report_timing -max_paths 2 -nworst 10}
+redirect  -tee -file ${REPORT_PATH}/report_timing_attribute.txt   {report_timing -attribute}
+redirect  -tee -file ${REPORT_PATH}/report_timing_input_pins.txt  {report_timing -input_pins}
 redirect  -tee -file ${REPORT_PATH}/report_area.txt    {report_area}
+redirect  -tee -file ${REPORT_PATH}/report_cell.txt    {report_cell}
 redirect  -tee -file ${REPORT_PATH}/report_clock.txt   {report_clock -attributes -skew}
+redirect  -tee -file ${REPORT_PATH}/report_hierarchy.txt    {report_hierarchy}
+redirect  -tee -file ${REPORT_PATH}/report_lib.txt     {report_lib $LIB_NAME}
 redirect  -tee -file ${REPORT_PATH}/report_net.txt     {report_net -connections -verbose}
 redirect  -tee -file ${REPORT_PATH}/report_port.txt    {report_port -verbose}
+redirect  -tee -file ${REPORT_PATH}/report_power.txt   {report_power}
 redirect  -tee -file ${REPORT_PATH}/report_qor.txt     {report_qor}
 #检查错误信息
 #check_error -reset
@@ -270,32 +331,11 @@ redirect  -tee -file ${REPORT_PATH}/report_qor.txt     {report_qor}
 #=====================================================
 #report_constraint  -all_violators :把所有违例报告
 #report_timing  # 把路径报告出来
+#report_timing -attribute # 检查infeasible paths，优化时会忽略这些路径
 #report_timing -inputs_pins # 把带端口名的路径报告出来
 #report_timing -delay_type max # 把最差的路径报告出来
 #report_timing -max_paths 2 # 把不同分组的最差的2个路径报告出来
 #report_timing -max_paths 2 -nworst 2 # 把所有分组最差的2个路径报告出来
 #report_timing -signficant_digits 4 # 设置报告精度，这里到小数点后4位
 #report_timing -loops # 检查是否有组合逻辑环，否则会有latch
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
